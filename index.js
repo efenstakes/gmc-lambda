f090aa468bee1f95b3f4f1397a75dbe2a5bd92c0
Efen Stakes <efenstakes101@gmail.com>
2020-03-01 23:43:39 +0300

    add application attachments

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ var fs = require('fs')
 
 
 // include internal libraries
-const messager = require('./messanger')
+const messenger = require('./nm')
 
 
 
 // get environment vars from .env
 DotEnv.config()
+// const PORT = process.env.PORT || 3000
 const PORT = process.env.PORT || 3000
 
 
@@ -24,7 +25,6 @@ const app = Express()
 
 // log request info
 app.use(Morgan('dev'))
-
 
 // setup server to allow cor's
 app.use(Cors())
@@ -53,61 +53,54 @@ app.post('/mail', (req, res)=> {
 
 	// var form = new formidable.IncomingForm()
 	var form = formidable({ multiples: true })
-
 	form.parse(req, function (err, fields, files) {
       
         // fields about applicant
 		let {
-			applicant_fullname,
-			applicant_email,
-			applicant_phone,
-			applicant_title,
-			expectations,
-			applicant_dob,
-			company_name,
-			company_email,
-			company_phone,
+			applicant_fullname, applicant_email, applicant_phone,
+			applicant_title, applicant_dob, expectations,
+			company_name, company_email, company_phone,
 		} = fields
         // files --> cv and photo
         var cv = files.applicant_cv
         var photo = files.applicant_photo
 
-
-		console.log('fields ', fields)
+		// console.log('fields ', fields)
         console.log('files')
         console.log(cv)
-
         console.log('cv name ', cv.name,' path ', cv.path)
         console.log('photo name ', photo.name,' path ', photo.path)
-
-      
-	  //   fs.readFile(cv.path, 'Base64', function(err, contents) { 'utf8'
-
-		  	// if(err) return res.json(responze)
-		  	// console.log('contents')
-		   //  console.log(contents)
-	      
-		    // var mail = messager.make_mail(
-		    //                { name: fields.username, email: fields.email },
-		    //                null,
-		    //                cv,
-		    //                contents,
-		    //                photo ? photo : null
-		    //            )
-		    // console.log('send mail')
-
-			// mail.then((result) => {
-			//     console.log(result)
-			//   })
-			//   .catch((err) => {
-			//     console.log(err) //.message)
-			//   })
-	  //   })
-
+        
+        let mail_message = messenger.create_msg({
+				        	applicant_fullname,
+							applicant_email,
+							applicant_phone,
+							applicant_title,
+							expectations,
+							applicant_dob,
+							company_name,
+							company_email,
+							company_phone,
+				        })
+        // console.log('mail_message ', mail_message)
+        messenger.send_mail({
+	        	message: mail_message,
+	        	receiver: 'hypemap.co@gmail.com',
+	        	subject: 'APPLICATION',
+	        	// photo: photo.path,
+	        	photo: photo,
+	        	cv: cv,
+	        	error_callback: ()=> {
+	        		return res.json(responze)
+	        	},
+	        	success_callback: ()=> {
+	        		return res.json({ 'send': true })
+	        	}
+	        })
      
     })
 
-	res.json({ 'page': 'mail', 'message': 'send mail' })
+	// res.json({ 'page': 'mail', 'message': 'send mail' })
 })
 
 
@@ -116,3 +109,27 @@ app.post('/mail', (req, res)=> {
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`server started at port ${PORT}`)
 })
+
+// https://secret-scrubland-69885.herokuapp.com/      
+//   fs.readFile(cv.path, 'Base64', function(err, contents) { 'utf8'
+
+// if(err) return res.json(responze)
+// console.log('contents')
+//  console.log(contents)
+
+// var mail = messager.make_mail(
+//                { name: fields.username, email: fields.email },
+//                null,
+//                cv,
+//                contents,
+//                photo ? photo : null
+//            )
+// console.log('send mail')
+
+// mail.then((result) => {
+//     console.log(result)
+//   })
+//   .catch((err) => {
+//     console.log(err) //.message)
+//   })
+//   })
--- nm.js
+++ nm.js
@@ -34,7 +34,16 @@ const nodemailer = require("nodemailer")
 // }
 
 
-async function send_mail ({receiver, subject, message}) {
+exports.send_mail = async function send_mail ({
+        receiver, 
+        subject, 
+        message,
+        photo,
+        cv,
+        error_callback,
+        success_callback,
+    }) {
+    console.log('got photo ', photo)
 
     var transporter = nodemailer.createTransport({
         //   service: 'gmail',
@@ -48,31 +57,72 @@ async function send_mail ({receiver, subject, message}) {
         }
     });
       
-      var mailOptions = {
+    var mailOptions = {
         from: 'hypemap.co@gmail.com',
         to: receiver,
         subject: subject,
         html: message,
         attachments: [
-            {   // file on disk as an attachment
-                filename: 'klm-fonts.txt',
-                path: 'E:/apps/fonts.txt' // stream this file
-            }
+            // {   // file on disk as an attachment
+            //     filename: 'klm-fonts.txt',
+            //     path: 'E:/apps/fonts.txt'
+            // },
+            { filename: photo.name, path: photo.path, },
+            { filename: cv.name, path: cv.path, }
         ]
-      };
+    };
       
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+    try {
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log('transporter.sendMail ', error);
+              error_callback()
+            } else {
+              console.log('Email sent: ' + info.response);
+              success_callback()
+            }
+        });
+
+    } catch(e) {
+        print('block error ', e)
+        error_callback()
+    }
 
 } 
 
-send_mail({
-    receiver: 'efenstakes101@gmail.com', 
-    subject: 'GMC', 
-    message: '<h>Hello Again</b>'
-})
+// send_mail({
+//     receiver: 'efenstakes101@gmail.com', 
+//     subject: 'GMC', 
+//     message: '<h>Hello Again</b>'
+// })
+
+
+exports.create_msg = ({
+        applicant_fullname,
+        applicant_email,
+        applicant_phone,
+        applicant_title,
+        expectations,
+        company_name,
+        company_email,
+        company_phone,
+        applicant_dob,
+    }) => {
+    
+    return `
+        <h2> Applicant Details <h2>
+        <p> Name ${applicant_fullname} </p>
+        <p> Email ${applicant_email} </p>
+        <p> Phone ${applicant_phone} </p>
+        <p> Date of Birth ${applicant_dob} </p>
+        <p> Title ${applicant_title} </p>
+        <p> Expectations: </p>
+        <p> ${expectations} </p>
+
+        <h2> Company Details <h2>
+        <p> Name ${company_name} </p>
+        <p> Email ${company_email} </p>
+        <p> Phone ${company_phone} </p> 
+    `;
+}
